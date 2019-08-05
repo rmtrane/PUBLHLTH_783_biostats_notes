461388c11a4fda863585ca382a30f7e2a3e55afe
rmtrane <rmtrane@gmail.com>
2019-08-05 17:30:58 -0500

    Change output folder

--- _deploy.sh
+++ _deploy.sh
@@ -10,7 +10,7 @@ git config --global user.name "rmtrane"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
-cp -r ../_783_biostats_book/* ./
+cp -r _book/* ./
 git add --all *
 git commit -m "Update the book" || true
 git push -q origin gh-pages
