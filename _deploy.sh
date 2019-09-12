8c7b155327847850e0b82ec013a7990bb9b75674
rmtrane <rmtrane@gmail.com>
2019-09-12 13:21:12 -0500

    Updated _deploy.sh....again

--- _deploy.sh
+++ _deploy.sh
@@ -11,7 +11,7 @@ git config --global user.name "rmtrane"
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
-cp ../lectures ./
+cp -r ../lectures ./lectures
 git add --all *
 git commit -m "Update the book" || true
 git push -q origin gh-pages
